22870146776930ebe5c88f0689089bb19a7a4d67
farseen1982 <farseen1982@gmail.com>
2014-03-28 15:41:36 +0800

    first commit

---
+++ HomeMoney/src/com/farseen/homemoney/JournalActivity.java
@@ -0,0 +1,24 @@
+package com.farseen.homemoney;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ListView;
+
+public class JournalActivity extends Activity {
+	ListView listView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_journal_actvity);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.journal_actvity, menu);
+		return true;
+	}
+
+}
